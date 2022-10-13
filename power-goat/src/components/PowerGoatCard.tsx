437e8e493c7af5b4c0c16f4e629223a87633fe0f
jjenqvist <40454881+jjenqvist@users.noreply.github.com>
2022-10-13 15:37:14 +0300

    Added CardMedia and BillyTheGoat

--- power-goat/src/components/PowerGoatCard.tsx
+++ power-goat/src/components/PowerGoatCard.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import { Card, Stack, Typography } from '@mui/material';
-import powergoats from '../powergoats.json';
+import * as React from "react";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import { Card, CardMedia, Stack, Typography } from "@mui/material";
+import powergoats from "../powergoats.json";
+import BillyTheGoat from "../assets/BillyTheGoat.jpg";
 
 export interface PowerGoatCardProps {
   name: string;
@@ -23,19 +24,20 @@ export interface PowerGoatCardProps {
 const PowerGoatCard = ({ name, skill, story }: PowerGoatCardProps) => {
   return (
     <Card>
+      <CardMedia component="img" height="140" image={BillyTheGoat} alt="It is Billy!" />
       <CardContent>
         <Stack spacing={2}>
-          <Typography variant='h5' align='left'>
+          <Typography variant="h5" align="left">
             {name}
           </Typography>
-          <Typography variant='body2' align='left'>
+          <Typography variant="body2" align="left">
             {skill}
           </Typography>
-          <Typography variant='body2' align='left'>
+          <Typography variant="body2" align="left">
             {story}
           </Typography>
         </Stack>
-        <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
           <Button>Check the Goat</Button>
         </CardActions>
       </CardContent>
